525f0ac69ac392b5e3f0e76e96b038577a80b7ec
setiadevs <tegar.setiadi.smkn8@proton.me>
2025-05-16 22:13:47 +0700

    feat: Remove base path configuration for GitHub Pages deployment

--- svelte.config.js
+++ svelte.config.js
@@ -9,9 +9,6 @@ export default {
             fallback: 'index.html', // fallback untuk SPA
             strict: false // abaikan error prerender
         }),
-		paths: {
-			base: '/' // base path untuk deploy ke GitHub Pages
-		},
         // Prerender semua halaman yang bisa diprerender
         prerender: {
             entries: ['*'] // coba prerender semua route
