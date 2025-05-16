5d3bc7515fc8f281e18339c1243ad98b035fb139
setiadevs <tegar.setiadi.smkn8@proton.me>
2025-05-16 21:49:56 +0700

    feat: Add new entry points and node components for improved functionality
    - Introduced app.CIo4eMrU.js as the main entry point with extensive dependencies and component logic.
    - Created start.C3lbD51N.js to handle CSS loading and application startup.
    - Implemented node component 1.CWalBzCb.js, enhancing error handling and status management within the application.

--- svelte.config.js
+++ svelte.config.js
@@ -9,6 +9,9 @@ export default {
             fallback: 'index.html', // fallback untuk SPA
             strict: false // abaikan error prerender
         }),
+		paths: {
+			base: '/jwsholat' // base path untuk deploy ke GitHub Pages
+		},
         // Prerender semua halaman yang bisa diprerender
         prerender: {
             entries: ['*'] // coba prerender semua route
